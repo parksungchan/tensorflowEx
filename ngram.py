aa1e0e48315c9ac5b4e5cb6e24ad9edbe2223c78
parksungchan <soul8085@gmail.com>
2017-10-10 00:02:50 +0000

    Update : View: fileupload

--- ngram.py
+++ ngram.py
@@ -1,14 +1,14 @@
-import ngram
-
-set = ['joe item 1xd qz0123 protest'
-        ,'joe item  1xd qz0123 protest'
-        ,'joe item 1xd qz0  123 protest'
-        ,'joe it   em 1xd qz0123 protest'
-        ,'joe item 2xd qz0123 protest'
-        ,'joe item 2xd qz0243 protest' ]
-
-dataset = ngram.NGram(set)
-
-result = dataset.search('joe item 1xd qz0123 protest')
-
-print(result)
+# import ngram
+#
+# set = ['joe item 1xd qz0123 protest'
+#         ,'joe item  1xd qz0123 protest'
+#         ,'joe item 1xd qz0  123 protest'
+#         ,'joe it   em 1xd qz0123 protest'
+#         ,'joe item 2xd qz0123 protest'
+#         ,'joe item 2xd qz0243 protest' ]
+#
+# dataset = ngram.NGram(set)
+#
+# result = dataset.search('joe item 1xd qz0123 protest')
+#
+# print(result)
--- sublime.py
+++ sublime.py
@@ -1,16 +1,1 @@
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
-
-sudo apt-get update
-
-sudo apt-get install sublime-text-installer
-
-
-
-
-
-
-
-
-
-
-
+s
