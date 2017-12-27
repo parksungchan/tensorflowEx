7ecd9ae0d9e2b71e7c4e790a237e634ce2893ae4
parksungchan <soul8085@gmail.com>
2017-12-27 01:15:21 +0000

    bug fix : Image : Multi Thread

--- opencv.py
+++ opencv.py
@@ -59,3 +59,86 @@ cp ../opencv/opencv_contrib-3.2.0/modules/saliency/samples/computeSaliency.cpp .
 cp ../opencv/opencv-3.2.0/samples/data/Megamind.avi .
 g++ -o computeSaliency `pkg-config opencv --cflags` computeSaliency.cpp `pkg-config opencv --libs`
 ./computeSaliency FINE_GRAINED Megamind.avi 23
+
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential cmake git pkg-config
+sudo apt autoremove
+sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install libatlas-base-dev gfortran
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+sudo pip3 install virtualenv virtualenvwrapper
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+source ~/.bashrc
+mkvirtualenv cv    <-- library 추가 설치를 할 수 있다.
+sudo apt-get install python3.5-dev
+pip install numpy
+
+git clone https://github.com/Itseez/opencv.git
+cd /home/dev/opencv
+git checkout 3.0.0
+
+git clone https://github.com/Itseez/opencv_contrib.git
+cd /home/dev/opencv_contrib
+git checkout 3.0.0
+
+cd /home/dev/opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+	-D CMAKE_INSTALL_PREFIX=/usr/local \
+	-D INSTALL_C_EXAMPLES=ON \
+	-D INSTALL_PYTHON_EXAMPLES=ON \
+	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+	-D BUILD_EXAMPLES=ON ..
+
+make -j4
+sudo make install
+sudo ldconfig
+
+ls -l /usr/local/lib/python3.5/site-packages/
+
+cd ~/.virtualenvs/cv/lib/python3.5/site-packages/
+ln -s /usr/local/lib/python3.5/site-packages/cv2.cpython-35m-x86_64-linux-gnu.so cv2.so
+
+pip install tensorflow
+pip install matplotlib
+pip install wget
+pip install scipy
+pip install imutils
+pip install sklearn
+pip install requests
+########################################################################################################################
+workon cv
+python
+>>> import cv2
+>>> cv2.__version__
+'3.0.0'
+########################################################################################################################
+Last Your Python Interpreter Change
+/root/.virtualenvs/cv/lib/python3.5/site-packages
+
+
+
+
+참조 : https://www.pyimagesearch.com/2015/07/20/install-opencv-3-0-and-python-3-4-on-ubuntu/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
