98a7547a92f506d3e389a89e192dc37bcf858d33
parksungchan <soul8085@gmail.com>
2018-05-18 23:46:01 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,357 +1,47 @@
-import logging
-
-import keras
-import sys,resource
-from keras import optimizers
-from cluster.neuralnet.neuralnet_node import NeuralNetNode
-from cluster.neuralnet_model import resnet
-from cluster.neuralnet_model.nasnet import NASNetLarge
-from cluster.neuralnet_model.inception_v4 import inception_v4_model
-from master.workflow.netconf.workflow_netconf import WorkFlowNetConf
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ReduceLROnPlateau, CSVLogger, EarlyStopping
-from common.graph.nn_graph_manager import NeuralNetModel
-import tensorflow as tf
-import numpy as np
-from cluster.common.train_summary_info import TrainSummaryInfo
-from keras.utils import multi_gpu_model
-
-# from third_party.slim.train_image_classifier import TrainImageClassifier
-
-class NeuralNetNodeImage(NeuralNetNode):
-    def keras_get_model(self):
-        keras.backend.tensorflow_backend.clear_session()
-
-        try:
-            self.model = keras.models.load_model(self.last_chk_path)
-            logging.info("Train Restored checkpoint from:" + self.last_chk_path)
-        except Exception as e:
-            logging.info("None to restore checkpoint. Initializing variables instead." + self.last_chk_path)
-            logging.info(e)
-            if self.net_type == 'inceptionv4':
-                self.model = inception_v4_model(self.labels_cnt, 0.2, self.pretrain_model_path)
-                self.optimizer = optimizers.SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
-                self.optimizer = optimizers.Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=1e-6)
-            elif self.net_type == 'nasnet':
-                self.model = NASNetLarge(input_shape=(331, 331, 3))
-
-            elif self.net_type == 'resnet':
-                numoutputs = self.netconf["config"]["layeroutputs"]
-
-                if numoutputs == 18:
-                    self.model = resnet.ResnetBuilder.build_resnet_18((self.channel, self.x_size, self.y_size), self.labels_cnt)
-                elif numoutputs == 34:
-                    self.model = resnet.ResnetBuilder.build_resnet_34((self.channel, self.x_size, self.y_size), self.labels_cnt)
-                elif numoutputs == 50:
-                    self.model = resnet.ResnetBuilder.build_resnet_50((self.channel, self.x_size, self.y_size), self.labels_cnt)
-                elif numoutputs == 101:
-                    self.model = resnet.ResnetBuilder.build_resnet_101((self.channel, self.x_size, self.y_size), self.labels_cnt)
-                elif numoutputs == 152:
-                    self.model = resnet.ResnetBuilder.build_resnet_152((self.channel, self.x_size, self.y_size), self.labels_cnt)
-                elif numoutputs == 200:
-                    self.model = resnet.ResnetBuilder.build_resnet_200((self.channel, self.x_size, self.y_size), self.labels_cnt)
-
-        # # self.model = multi_gpu_model(self.model, gpus=4)
-        # if self.optimizer == 'sgd':
-        #     self.optimizer = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
-        #     # optimizers.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False)
-        # elif self.optimizer == 'rmsprop':
-        #     self.optimizer = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
-        # elif self.optimizer == 'adagrad':
-        #     self.optimizer = optimizers.Adagrad(lr=0.01, epsilon=1e-08, decay=0.0)
-        # elif self.optimizer == 'adadelta':
-        #     self.optimizer = optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0)
-        # elif self.optimizer == 'adam':
-        #     self.optimizer = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-        # elif self.optimizer == 'adamax':
-        #     self.optimizer = optimizers.Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-        # elif self.optimizer == 'nadam':
-        #     self.optimizer = optimizers.Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.004)
-        #
-        # self.optimizer = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
-        #
-        # self.model.compile(loss='categorical_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
-
-
-        self.model.compile(optimizer=self.optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-
-    def train_run_image(self, input_data, test_data):
-        '''
-        Train Run
-        :param input_data:
-        :param test_data:
-        :return:
-        '''
-        self.epoch = self.netconf["param"]["epoch"]
-        self.data_augmentation = self.netconf["param"]["augmentation"]
-        try:
-            self.fit_size = self.netconf["param"]["fit_size"]
-        except:
-            self.fit_size = 9999999999
-
-        self.lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-6)
-        self.early_stopper = EarlyStopping(monitor='val_acc', min_delta=0.001, patience=10)
-
-        try:
-            while_cnt = 0
-            self.loss = 0
-            self.acc = 0
-            self.val_loss = 0
-            self.val_acc = 0
-
-            input_data.reset_pointer()
-            test_data.reset_pointer()
-
-            test_set = test_data[0:test_data.data_size()]
-            x_tbatch = self.get_convert_img_x(test_set[0], self.x_size, self.y_size, self.channel) # img_data_batch
-            y_tbatch = self.get_convert_img_y(test_set[1], self.labels, self.labels_cnt) # label_data_batch
-
-            while (input_data.has_next()):
-                run_size = 0
-                while( run_size < input_data.data_size()):
-                    if run_size + self.fit_size > input_data.data_size():
-                        input_set = input_data[run_size:input_data.data_size()]
-                    else:
-                        input_set = input_data[run_size:run_size + self.fit_size]
-                    run_size += self.fit_size + 1
-                    x_batch = self.get_convert_img_x(input_set[0], self.x_size, self.y_size, self.channel)  # img_data_batch
-                    y_batch = self.get_convert_img_y(input_set[1], self.labels, self.labels_cnt)  # label_data_batch
-
-                    if self.data_augmentation == "N" or self.data_augmentation == "n":
-                        history = self.model.fit(x_batch, y_batch,
-                                                 batch_size=self.batch_size,
-                                                 epochs=self.epoch,
-                                                 validation_data=(x_tbatch, y_tbatch),
-                                                 shuffle=True,
-                                                 callbacks=[self.lr_reducer, self.early_stopper])
-                    else:
-                        # This will do preprocessing and realtime data augmentation:
-                        datagen = ImageDataGenerator(
-                            featurewise_center=False,  # set input mean to 0 over the dataset
-                            samplewise_center=False,  # set each sample mean to 0
-                            featurewise_std_normalization=False,  # divide inputs by std of the dataset
-                            samplewise_std_normalization=False,  # divide each input by its std
-                            zca_whitening=False,  # apply ZCA whitening
-                            rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-                            width_shift_range=0.1, # randomly shift images horizontally (fraction of total width)
-                            height_shift_range=0.1, # randomly shift images vertically (fraction of total height)
-                            horizontal_flip=True,  # randomly flip images
-                            vertical_flip=False)  # randomly flip images
-
-                        # Compute quantities required for featurewise normalization
-                        # (std, mean, and principal components if ZCA whitening is applied).
-                        datagen.fit(x_batch)
-
-                        # Fit the model on the batches generated by datagen.flow().
-                        history = self.model.fit_generator(
-                            datagen.flow(x_batch, y_batch, batch_size=self.batch_size),
-                            steps_per_epoch=x_batch.shape[0] // self.batch_size,
-                            validation_data=(x_tbatch, y_tbatch),
-                            epochs=self.epoch, verbose=1, max_q_size=100,
-                            callbacks=[self.lr_reducer, self.early_stopper, self.csv_logger])
-
-                    self.loss += history.history["loss"][len(history.history["loss"])-1]
-                    self.acc += history.history["acc"][len(history.history["acc"])-1]
-                    self.val_loss += history.history["val_loss"][len(history.history["val_loss"])-1]
-                    self.val_acc += history.history["val_acc"][len(history.history["val_acc"])-1]
-
-                    while_cnt += 1
-                input_data.next()
-
-            if while_cnt > 0:
-                self.loss =self.loss/while_cnt
-                self.acc = self.acc / while_cnt
-                self.val_loss = self.val_loss / while_cnt
-                self.val_acc = self.val_acc / while_cnt
-
-        except Exception as e:
-            logging.info("Error[400] ..............................................")
-            logging.info(e)
-
-    def run(self, conf_data):
-        '''
-        Train run init
-        :param conf_data: 
-        :return: 
-        '''
-        try :
-            logging.info("run NeuralNetNodeImage Train")
-            # Common Start #############################################################################################
-            # init value
-            self = NeuralNetNode()._init_node_parm(self, conf_data)
-
-            # netconf
-            self.netconf = WorkFlowNetConf().get_view_obj(self.node_id)
-
-            # dataconf & get data
-            input_data, self.dataconf = self.get_input_data(self.feed_node, self.cls_pool, self.train_feed_name)
-            test_data, self.dataconf_eval = self.get_input_data(self.feed_node, self.cls_pool, self.eval_feed_name)
-            # Common End ###############################################################################################
-
-            # Label Setup (1: HDF label row)
-            self.labels, self.labels_cnt = self._get_netconf_labels(self.netconf, input_data, 1)
-
-            self.channel = self.dataconf["preprocess"]["channel"]
-            self.x_size = self.dataconf["preprocess"]["x_size"]
-            self.y_size = self.dataconf["preprocess"]["y_size"]
-            self.train_cnt = self.netconf["param"]["traincnt"]
-            self.batch_size = self.netconf["param"]["batch_size"]
-            self.predlog = self.netconf["param"]["predictlog"]
-            self.optimizer = self.netconf["config"]["optimizer"]
-
-            # get model
-            self.keras_get_model()
-
-            for i in range(self.train_cnt):
-                # Train
-                self.train_run_image(input_data, test_data)
-
-                # Model Save :  _init_node_parm : self.save_path
-                keras.models.save_model(self.model, self.save_path)
-
-                # Acc Loss Save : _init_node_parm : self.acc_loss_result
-                self.set_acc_loss_result(self.acc_loss_result, self.loss, self.acc, self.val_loss, self.val_acc)
-
-                # Eval & Result Save
-                self.eval(self.node_id, self.conf_data, test_data, None)
-
-                # Eval Result Print
-                self.eval_result_print(self.eval_data, self.predlog)
-
-            if self.train_cnt == 0:
-                # Eval & Result Save
-                self.eval(self.node_id, self.conf_data, test_data, None)
-
-                # Eval Result Print
-                self.eval_result_print(self.eval_data, self.predlog)
-
-            return None
-        except Exception as e :
-            logging.info("===Error on Train  : {0}".format(e))
-
-    ####################################################################################################################
-    def eval(self, node_id, conf_data, data=None, result=None):
-        '''
-        eval run init
-        :param node_id: 
-        :param conf_data: 
-        :param data: 
-        :param result: 
-        :return: 
-        '''
-        try :
-            logging.info("run NeuralNetNodeImage eval")
-
-            pred_cnt = self.netconf["param"]["predictcnt"]
-            eval_type = self.netconf["config"]["eval_type"]
-
-            # eval result
-            config = {"type": eval_type, "labels": self.labels,
-                      "nn_id": self.nn_id,
-                      "nn_wf_ver_id": self.nn_wf_ver_id, "nn_batch_ver_id": self.train_batch}
-            self.eval_data = TrainSummaryInfo(conf=config)
-
-            if data is None:
-                return self.eval_data
-
-            data.reset_pointer()
-
-            while (data.has_next()):
-                data_set = data[0:data.data_size()]
-                x_batch = self.get_convert_img_x(data_set[0], self.x_size, self.y_size, self.channel)  # img_data_batch
-                logits = self.model.predict(x_batch)
-
-                y_batch = self.get_convert_img_y_eval(data_set[1])
-                n_batch = self.get_convert_img_y_eval(data_set[2]) # File Name
-
-                for i in range(len(logits)):
-                    true_name = y_batch[i]
-
-                    logit = []
-                    logit.append(logits[i])
-                    retrun_data = self.set_predict_return_cnn_img(self.labels, logit, pred_cnt)
-                    pred_name = retrun_data["key"]
-                    pred_value = retrun_data["val"]
-                    #예측값이 배열로 넘어온다 한개라도 맞으면참
-                    t_pred_name = pred_name[0]
-                    for pred in pred_name:
-                        if pred == true_name:
-                            t_pred_name = pred
-
-                    # eval result
-                    self.eval_data.set_result_info(true_name, t_pred_name)
-
-                    # Row log를 찍기위해서 호출한다.
-                    file_name = n_batch[i]
-                    self.eval_data.set_tf_log(file_name, true_name, pred_name, pred_value)
-
-                data.next()
-
-            # eval result
-            if self.train_cnt != 0:
-                TrainSummaryInfo.save_result_info(self, self.eval_data)
-
-            return self.eval_data
-
-        except Exception as e :
-            logging.info("===Error on Eval  : {0}".format(e))
-
-    ####################################################################################################################
-    def predict(self, nn_id, ver, filelist):
-        '''
-        predict
-        :param node_id: 
-        :param filelist: 
-        :return: 
-        '''
-        logging.info("run NeuralNetNodeImage Predict")
-        self = NeuralNetNode()._init_pred_parm(self, nn_id, ver)
-        # net   config setup
-        self.netconf = WorkFlowNetConf().get_node_info(nn_id, ver, self.netconf_name)
-        self.dataconf = WorkFlowNetConf().get_node_info(nn_id, ver, self.dataconf_name)
-
-        # data shape change MultiValuDict -> nd array
-        filename_arr, filedata_arr = self.change_predict_fileList(filelist, self.dataconf)
-
-        # get unique key
-        unique_key = '_'.join([str(nn_id), str(ver), self.load_batch])
-
-        logging.info("getModelPath:"+self.model_path + "/" + self.load_batch+self.file_end)
-
-        ## create tensorflow graph
-        if (NeuralNetModel.dict.get(unique_key)):
-            self = NeuralNetModel.dict.get(unique_key)
-            graph = NeuralNetModel.graph.get(unique_key)
-        else:
-            self.keras_get_model()
-
-            NeuralNetModel.dict[unique_key] = self
-            NeuralNetModel.graph[unique_key] = tf.get_default_graph()
-            graph = tf.get_default_graph()
-
-        pred_return_data = {}
-        for i in range(len(filename_arr)):
-            file_name = filename_arr[i]
-            file_data = filedata_arr[i]
-
-            try:
-                logits = self.model.predict(file_data)
-            except Exception as e:
-                self.keras_get_model()
-
-                NeuralNetModel.dict[unique_key] = self
-                NeuralNetModel.graph[unique_key] = tf.get_default_graph()
-                graph = tf.get_default_graph()
-                logits = self.model.predict(file_data)
-
-            labels = self.netconf["labels"]
-            pred_cnt = self.netconf["param"]["predictcnt"]
-            retrun_data = self.set_predict_return_cnn_img(labels, logits, pred_cnt)
-            pred_return_data[file_name] = retrun_data
-            logging.info("Return Data.......................................")
-            logging.info(pred_return_data)
-
-        return pred_return_data
-
-    ####################################################################################################################
-
+1일차
+올래국수:제주도 고기국수 맛집
+삼무국수:제주도 고기국수 맛집
+자매국수:제주도 고기국수 맛집 삼대천왕 나옴
+덤장:갈치구이
+
+함덕해수용장위치
+델문도 카페 디저트
+바람벽에흰당나귀카페,디저트
+쪼끌락카페,디저트 김녕라떼로 유명한 제주도 디저트카페
+
+백리향:백반집 7000 갈치구이 13000
+제주또바기:보말칼국수
+오드랑베이커리:빵
+버드나무집:보말칼구수 목요일휴무 10000 해물손칼국수
+해녀촌:회국수가 유명 9000
+월정해녀식당:통갈치구이45000
+함덕밀면국수 9000, 흑돼지연탄한판13000
+
+
+롯데호텔
+바다다카페,디저트
+카페세렌디카페,디저트
+이정의댁카페
+옐로우카페카페,디저트
+르피크닉카페,디저트
+
+한돈가 100그람 10000
+오르막가든 흑돼지 17000
+중문흑돼지 190 18000
+쉬는팡가든 200 18000
+제주흑도새기 200 18000
+꺼멍목장 200 19000
+참이쁜돼지 200  19000
+
+
+
+국수바다본점 고기국수8000
+영실국수 8000 해물짬뽕15000
+중문수두리보말칼국수 9000
+중문국수마을국수 7000
+제주한라국수 국수 8000
+
+제주미향 갈치구이 45000
+
+피제리아3657피자28000
+뀌에떼이탈리아음식16000
